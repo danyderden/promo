37723ad6e6772fad2a5e04206e7c64204f4a487d
danyderden <danyderden@gmail.com>
2023-07-09 00:52:41 +0300

    fix url in env

--- migrations/env.py
+++ migrations/env.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import pool
@@ -31,6 +32,8 @@ target_metadata = PromoCode.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+url = os.getenv('DB_URL', config.get_main_option("sqlalchemy.url"))
 
 
 def run_migrations_offline() -> None:
