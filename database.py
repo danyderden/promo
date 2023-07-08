61aef7603fc028dbedf92d8d23a7167371ccf640
danyderden <danyderden@gmail.com>
2023-07-09 00:46:24 +0300

    create async migration

--- database.py
+++ database.py
@@ -5,7 +5,6 @@ from typing import AsyncGenerator
 
 from config import DB_URL
 
-# DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 DATABASE_URL = DB_URL
 
 
