1e2ccc7f2db155dd40840f2f30e8671282d6fdb5
danyderden <danyderden@gmail.com>
2023-07-09 01:09:36 +0300

    delete main in main

--- main.py
+++ main.py
@@ -4,7 +4,3 @@ from fastapi import FastAPI
 from outputter.handlers import promo_code_router
 app = FastAPI(title='Promo code out putter')
 app.include_router(promo_code_router)
-
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True, workers=1, env_file='.env')
